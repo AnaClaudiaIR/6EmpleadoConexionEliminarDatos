7ff3baa03590455d7555f098f2df87d4a12daa4e
AnaClaudiaIR <anac.ihnrod@educa.jcyl.es>
2026-03-25 12:08:59 +0100

    subir2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,32 +2,25 @@ package org.example;
 
 import java.sql.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args) {
         try (Connection connection = DriverManager.getConnection(
                 DBConfig.getUrl(),
                 DBConfig.getUser(),
                 DBConfig.getPassword()
-        ); Statement statement = connection.createStatement()) {
-            //Preparar el String para insertar datos
-            String sql = "INSERT INTO empleado(id, nombre, salario) VALUES (?,?,?)";
-            //statement.executeUpdate(sql);
+        )) {
+            String sql = "UPDATE empleado SET nombre = ?, salario = ? WHERE id = ?";
 
-            //Preparar el statement para introducir los datos
+            //Introducir en el statement los parámetros
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "Kiro");
+            preparedStatement.setDouble(2,2500.67);
+            preparedStatement.setInt(3,1);
+            int filasActualizadas = preparedStatement.executeUpdate(); //Mostrar número de filas afectadas
+            System.out.println("Datos actualizados. Filas afectadas: " + filasActualizadas);
 
-            //Se introducen en el orden asignado en el String
-            preparedStatement.setInt(1, 6);
-            preparedStatement.setString(2, "Sabrina");
-            preparedStatement.setDouble(3, 5.67);
-            preparedStatement.executeUpdate();
-            System.out.println("Registro insertado correctamente.");
-
-            /* int n = ps.executeUpdate
-            * sout(n) --> Mostrar número de líneas afectadas*/
-
-        } catch (SQLException e) {
-            System.out.println("ERROR --> " + e.getMessage());
+        }catch (SQLException e){
+            System.out.println("Error --> "+e.getMessage());
         }
     }
 }
